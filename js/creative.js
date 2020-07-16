5fc4e2e8e5159044ba9d3d6d5137bbdd9dd8f327
Wendy <11175050+wendy410@users.noreply.github.com>
2020-07-17 07:53:53 +0900

    Add waifu

--- js/creative.js
+++ js/creative.js
@@ -52,6 +52,76 @@
         });
     })
 
+    $('#zui').on('click', function () {
+        $.fancybox.open([
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E7%91%9E%E9%B3%B3%E6%94%B9%E4%BA%8C%E4%B9%99/::ref/360_2nd.jpg',
+                opts: {
+                    caption: '<p>「ぎゅぅう～っと、もっと頑張ります！」</p>'
+                }
+            },
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E7%91%9E%E9%B3%B3/::ref/112_2nd.jpg',
+                opts: {
+                    caption: '<p>「瑞鳳です。軽空母ですが、練度があがれば、正規空母並の活躍をおみせできます。」</p>'
+                }
+            },
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E7%91%9E%E9%B3%B3%E6%94%B9/::ref/113_2nd.jpg',
+                opts: {
+                    caption: '<p>♡♡おべんと、食べる？♡♡</p>'
+                }
+            },
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E7%91%9E%E9%B3%B3%E6%94%B9%E4%BA%8C/::ref/355_2nd.jpg',
+                opts: {
+                    caption: '<p>「大規模改装、しちゃいました！」</p>'
+                }
+            }
+        ], {
+            loop: true,
+            buttons: [
+                "fullScreen",
+                "close"
+            ],
+            protect: true,
+            transitionEffect: "tube",
+            transitionDuration: 500
+        });
+    });
+
+    $('#sg').on('click', function () {
+        $.fancybox.open([
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E6%99%82%E9%9B%A8%E6%94%B9%E4%BA%8C/::ref/145_2nd.jpg',
+                opts: {
+                    caption: '<p>「ありがとう。少し、強くなれたみたいだ。」</p>'
+                }
+            },
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E6%99%82%E9%9B%A8/::ref/080_2nd.jpg',
+                opts: {
+                    caption: '<p>「僕は白露型駆逐艦、時雨。これからよろしくね。」</p>'
+                }
+            },
+            {
+                src: 'https://cdn.wikiwiki.jp/to/w/kancolle/%E6%99%82%E9%9B%A8%E6%94%B9/::ref/080b_2nd.jpg',
+                opts: {
+                    caption: '<p>「僕に興味があるの？...いいよ、何でも聞いてよ。」</p>'
+                }
+            }
+        ], {
+            loop: true,
+            buttons: [
+                "fullScreen",
+                "close"
+            ],
+            protect: true,
+            transitionEffect: "zoom-in-out",
+            transitionDuration: 500
+        });
+    });
+
     // $('#watch').on({
     //     'mouseenter': function () {
 
